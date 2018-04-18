d04bcd0fdc14254de75047aa4c66a96bf1a65252
maphihung <phihung.3497@gmail.com>
2018-04-18 10:15:37 +0700

    BaiThucHanh3_18_4_2018

---
+++ BaiTH3/src/BaiTH3.java
@@ -0,0 +1,14 @@
+
+
+public class BaiTH3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            new main().setVisible(true);
+            //new Student().setVisible(true);
+        }
+}
+        
